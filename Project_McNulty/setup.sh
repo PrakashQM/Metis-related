626137e0bdb832b2a5a2ff36b63feb6b7956d373
prakashQM <electronicstructurelab@gmail.com>
2018-05-16 11:21:35 -0700

    Project McNulty

---
+++ Project_McNulty/setup.sh
@@ -0,0 +1,6 @@
+echo "# Project__McNulty" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/PrakashQM/Project__McNulty.git
+git push -u origin master
